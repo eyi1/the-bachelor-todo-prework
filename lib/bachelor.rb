af0557ec33b1390a7b1c704162895a1207781608
eyi1 <ej7729@gmail.com>
2018-03-30 03:36:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -26,6 +26,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
+  how_many_same_hometown = 0 
+    data.each do |season, info|
+      info.each do |people|
+        people.each do |key, value|
+          if value == hometown
+            how_many_same_hometown += 1 
+          end
+        end
+      end
+    end
+    how_many_same_hometown   
   # code here
 end
 
